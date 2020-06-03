659d886989ac8c897e66e2a8718034f695a0591e
Brian <bgome004@gmail.com>
2020-06-03 09:19:45 -0400

    Updated links

--- contact.php
+++ contact.php
@@ -148,23 +148,23 @@ an error sending your message. Please try again later.</div>';
 
     </div><!--end navbar-header-->
 
-    <div class="collapse navbar-collapse" align="center" >
+     <div class="collapse navbar-collapse" align="center">
+        <ul class="nav navbar-nav">
+          <li>
+            <a href="accounting-services.html">ACCOUNTING SERVICES</a>
+          </li>
 
-      <ul class="nav navbar-nav">
-          
-          <li><a href="http://yourwebsitename.com/accounting-services.html">ACCOUNTING SERVICES</a></li>
+          <li>
+            <a href="accounting-resources.html">RESOURCES</a>
+          </li>
 
-          <li><a href="http://yourwebsitename.com/accounting-resources.html">RESOURCES</a></li>
+          <li><a href="blog.html">BLOG</a></li>
 
-          <li><a href="http://yourwebsitename.com/blog.html">BLOG</a></li>
+          <li><a href="about.html">ABOUT</a></li>
 
-          <li><a href="http://yourwebsitename.com/about.html">ABOUT</a></li>
-
-          <li><a href="http://yourwebsitename.com/contact.php">CONTACT</a></li>
-
-      </ul>
-
-    </div>
+          <li><a href="contact.php">CONTACT</a></li>
+        </ul>
+      </div>
 
   </nav><!--end navbar-->
 
@@ -285,37 +285,39 @@ an error sending your message. Please try again later.</div>';
   </div><!--end consultation-strip-->
 
 
+ 
   <footer>
+      <div id="footer" align="center">
+        <a href="about.html">ABOUT</a>
 
-    <div id="footer" align="center">
+        <a href="accounting-services.html">ACCOUNTING SERVICES</a>
 
-        <a href="http://yourwebsitename.com/about.html">ABOUT</a>
+        <a href="accounting-resources.html">RESOURCES</a>
 
-        <a href="http://yourwebsitename.com/accounting-services.html">ACCOUNTING SERVICES</a> 
+        <a href="blog.html">BLOG</a>
 
-        <a href="http://yourwebsitename.com/accounting-resources.html">RESOURCES</a> 
+        <a href="faq.html">FAQ</a>
 
-        <a href="http://yourwebsitename.com/blog.html">BLOG</a> 
+        <a href="contact.php">CONTACT</a>
+      </div>
+      <!--end of footer-->
 
-        <a href="http://yourwebsitename.com/faq.html">FAQ</a> 
+      <p class="address">
+        Address: 123 Random Ave, Wake Forest, NC 27587
+      </p>
 
-        <a href="http://yourwebsitename.com/contact.php">CONTACT</a>
+      <div class="footerlinks" align="center">
+        <a href="privacy-policy.html" rel="nofollow">Privacy Policy</a>
 
-    </div><!--end of footer-->
+        <a href="terms-and-conditions.html" rel="nofollow"
+          >Terms &amp; Conditions</a
+        >
 
-            <p class="address">Address: 7 Random Avenue, Pearl Street, Cleveland, 0020</p>
-
-    <div class="footerlinks" align="center">
-      
-      <a href="http://yourwebsitename.com/privacy-policy.html" rel="nofollow">Privacy Policy</a> 
-
-      <a href="http://yourwebsitename.com/terms-and-conditions.html" rel="nofollow">Terms &amp; Conditions</a>
-
-      <p align="center" class="text-muted copyright">&copy; Your Website/Business 2017</p>
-
-    </div>
-
-  </footer><!--end Footer-->
+        <p align="center" class="text-muted copyright">
+          &copy; Your Website/Business 2020
+        </p>
+      </div>
+    </footer><!--end Footer-->
 
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
